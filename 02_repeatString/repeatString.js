abe8b572db669dd3d7cf6ff5f70e3d4bc52a808e
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-07-21 12:19:06 -0600

    complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let phrase = ''
+    if (num < 0) {
+        phrase += "ERROR"
+    } else {
+        for (i = num; i > 0; i--) {
+         phrase += string;
+        }
+    }
+    return phrase;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
