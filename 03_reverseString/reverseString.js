729b6d8c04569fd13b464b97d07130a269145e19
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-07-21 12:33:50 -0600

    complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    letters = string.toString().split("");
+    reverse = letters.reverse().join("");
+    return reverse;
 };
 
 // Do not edit below this line
