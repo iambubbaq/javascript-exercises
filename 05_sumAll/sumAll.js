e8673a03a65f6d9fe45c72809dfe8b96ca97eb78
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-08-03 12:58:22 -0600

    Answer excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    if (a > b) {
+        let newA = a;
+        a = b;
+        b = newA;
+    };
 
+    let sum = 0;
+    for (i = a; i < b + 1; i++) {
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
