ea7662bf32d0b9765b0f009071321b8aed9ceb0e
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-08-23 15:06:37 -0600

    Add solution to excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.sort(function(a, b) {
+        let prev;
+        let next;
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+            prev = a.yearOfDeath - a.yearOfBirth;
+            next = b.yearOfDeath - b.yearOfBirth;
+            return prev > next ? -1 : 1;
+        } else if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+            prev = a.yearOfDeath - a.yearOfBirth;
+            next = b.yearOfDeath - b.yearOfBirth;
+            return prev > next ? -1 : 1;
+        } else {
+            prev = a.yearOfDeath - a.yearOfBirth;
+            next = b.yearOfDeath - b.yearOfBirth;
+            return prev > next ? -1 : 1;
+        }
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
