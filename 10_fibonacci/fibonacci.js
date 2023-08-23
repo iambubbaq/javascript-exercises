f7799734a4e49340df82398bf4e898a395dcfdad
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-08-23 14:03:59 -0600

    Add solution to excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let prev1 = 0;
+    let prev2 = 1;
+    if (num < 0) {
+        return "OOPS";
+    } else if (num === 0) {
+        return 0;
+    } else {
+        for (let i = 2; i <= num; i++) {
+            let current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
 
+        }
+    }
+    return prev2;
 };
 
 // Do not edit below this line
