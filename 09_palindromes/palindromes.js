13db972b3647221f5a1eee6d7f90085ff63520a7
Stephen Williams <25148210+iambubbaq@users.noreply.github.com>
2023-08-23 13:26:03 -0600

    Add solutions for excercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let initial = phrase.replace(/[\.,?! ]/g, "").split("").join("").toLowerCase();
+    let reverse = initial.split("").reverse().join("");;
+    return initial === reverse;
 };
 
 // Do not edit below this line
